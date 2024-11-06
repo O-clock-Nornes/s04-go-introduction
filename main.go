9ba52a33f670fc8d4d71cad73bb6ee577c6ee7f2
Aurelien <aurelien.bras@gmail.com>
2024-11-06 14:14:21 +0000

    petit manip simple de fichier

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
-import "log"
-
-var (
-	token = "c732a4f732342956ec521490b59a7dce"
+import (
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	ville := "najac"
 
-	coord, err := getCoordFromCity(ville)
+	// D'abord on ouvre le fichier test.txt
+	f, err := os.Open("test.txt")
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
-	log.Printf("%#v", coord)
+	defer f.Close()
+	// Ici on le lit
+	data, _ := io.ReadAll(f)
+	log.Printf("%v", string(data))
+
+	// On fait un nouveau fichier avec le résultat de notre lecture
+	os.WriteFile("test-copie.txt", data, 0600)
+
 }
